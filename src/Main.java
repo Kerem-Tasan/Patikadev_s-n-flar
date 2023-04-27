dee75434b15aee90add50fea4862eaa1ca04e0e7
Kerem-Tasan <128915083+Kerem-Tasan@users.noreply.github.com>
2023-04-27 14:59:04 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,10 +14,12 @@ public class Main {
 
 
         Student s1 = new Student("Kerem", "123", "4", tarih, fizik, biolgy);
+        s1.addSozluNote(50,50,50);
         s1.addBulExamNote(100, 40, 90);
         s1.isPass();
 
         Student s2=new Student("Samet","321","4",tarih,fizik,biolgy);
+        s2.addSozluNote(70,70,90);
         s2.addBulExamNote(90,80,100);
         s2.isPass();
     }
