3790c5289f67a66cb38b84f5709181e308ba6cae
Kerem-Tasan <128915083+Kerem-Tasan@users.noreply.github.com>
2023-04-30 15:50:25 +0300

    Add files via upload

---
+++ maashesap.src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee w1 = new Employee("Kerem", "Taşan", 20000, 50, 2000);
+        System.out.println("====Hoşgeldiniz====");
+
+        w1.addSalary();
+    }
+}
